1a583171730c6bce9f094dbe95e4f1b7f9bce166
ivaylosharkov <ivaylo.sharkov@scalefocus.com>
2018-02-26 08:55:09 -0600

    Update post_deploy.sh
    Added silent confirmation for apt-get

--- post_deploy.sh
+++ post_deploy.sh
@@ -3,14 +3,14 @@
 #post deployment configuration:
 
 ## add docker ce
-apt-get install   apt-transport-https     ca-certificates     curl     software-properties-common
+apt-get install   apt-transport-https     ca-certificates     curl     software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 apt-key fingerprint 0EBFCD88
 add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 apt-get update
-apt-get install docker-ce
+apt-get install docker-ce -i
 
 groupadd docker
 usermod -aG docker ubuntu
